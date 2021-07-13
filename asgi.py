f01d0cdf0fd0bae02568c0800f1a56246a33ba1a
Fábio Rolim <fabiorolim@MacBook-Pro-de-Fabio-2.local>
2021-07-13 14:54:56 -0300

    Refactor name project

--- asgi.py
+++ asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lol project.
+ASGI config for lol-contas project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lol project.
+WSGI config for lol-contas project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
